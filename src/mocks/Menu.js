81dfef8c2801b39584fd2aed23f32249959c8432
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2023-08-15 19:31:34 -0300

    menu and order

---
+++ src/mocks/Menu.js
@@ -0,0 +1,37 @@
+export const menu = [
+  {
+    name: 'Hambúrguer Clássico',
+    price: 15.99,
+    prepTime: '10 minutos'
+  },
+  {
+    name: 'Batata Frita',
+    price: 7.50,
+    prepTime: '5 minutos'
+  },
+  {
+    name: 'Cheeseburger',
+    price: 17.50,
+    prepTime: '12 minutos'
+  },
+  {
+    name: 'Cachorro-Quente',
+    price: 12.00,
+    prepTime: '8 minutos'
+  },
+  {
+    name: 'Milkshake de Chocolate',
+    price: 9.99,
+    prepTime: '5 minutos'
+  },
+  {
+    name: 'Salada',
+    price: 13.50,
+    prepTime: '8 minutos'
+  },
+  {
+    name: 'Sundae de Morango',
+    price: 6.50,
+    prepTime: '3 minutos'
+  }
+];
--- src/utils/order.js
+++ src/utils/order.js
@@ -3,4 +3,43 @@ class Order {
   #items;
   #preparationTime;
   #finalizationTime;
+
+  constructor(_number, _items, _preparationTime, _finalizationTime) {
+    this.#number = _number;
+    this.#items = _items;
+    this.#preparationTime = _preparationTime;
+    this.#finalizationTime = _finalizationTime;
+  }
+
+  getNumber() {
+    return this.#number;
+  }
+
+  setNumber(_newNumber) {
+    this.#number = _newNumber;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  setItems(_newItems) {
+    this.#number = _newItems;
+  }
+
+  getPreparationTIme() {
+    return this.#preparationTime;
+  }
+
+  setPreparationTime(_newPreparationTime) {
+    this.#preparationTime = _newPreparationTime;
+  }
+
+  getFinalizationTime() {
+    return this.#finalizationTime;
+  }
+
+  setFinalizationTime(_newFinalizationTime) {
+    this.#finalizationTime = _newFinalizationTime;
+  }
 }
